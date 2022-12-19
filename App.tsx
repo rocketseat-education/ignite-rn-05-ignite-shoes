7c6bd3b8c01ceb117057372ae581583ac2467654
RennanD <rplaykl@gmail.com>
2022-12-19 17:23:21 -0300

    OneSignal - Instalando o OneSignal
    OneSignal - Instalando o OneSignal

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +9,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId('dd6753a9-19ad-4f0f-b1c3-700cff7f9dcc')
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
