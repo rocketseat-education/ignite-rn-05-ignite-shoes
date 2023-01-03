38bcbd4fcdfb83fcbc9d3b17ad46dee7f555a8a1
RennanD <rplaykl@gmail.com>
2023-01-03 18:14:08 -0300

    Apple Push Notification Service - Executando e testando a integração

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'react-native';
+import { StatusBar, Platform } from 'react-native';
 import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
@@ -10,7 +10,12 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
-OneSignal.setAppId('dd6753a9-19ad-4f0f-b1c3-700cff7f9dcc')
+const oneSignalAppId = Platform.OS === 'ios' ? '2b542c58-e2e9-48f1-8bff-004caefe40c4' : 'dd6753a9-19ad-4f0f-b1c3-700cff7f9dcc';
+OneSignal.setAppId(oneSignalAppId);
+
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+  console.log(response);
+})
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
