ef3767ebbb61dc6b8b5f93df46e275bd3df6467d
RennanD <rplaykl@gmail.com>
2023-01-05 10:31:12 -0300

    Push Notifications - Recebendo mensagem em foreground

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
+import { useEffect } from 'react';
 import { StatusBar, Platform } from 'react-native';
-import OneSignal from 'react-native-onesignal';
+import OneSignal, { NotificationReceivedEvent } from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -14,14 +15,22 @@ import { CartContextProvider } from './src/contexts/CartContext';
 const oneSignalAppId = Platform.OS === 'ios' ? '2b542c58-e2e9-48f1-8bff-004caefe40c4' : 'dd6753a9-19ad-4f0f-b1c3-700cff7f9dcc';
 OneSignal.setAppId(oneSignalAppId);
 
-OneSignal.promptForPushNotificationsWithUserResponse(response => {
-  console.log(response);
-})
+OneSignal.promptForPushNotificationsWithUserResponse()
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate();
+
+  useEffect(() => {
+    const unsubscribe = OneSignal
+    .setNotificationWillShowInForegroundHandler((notificationRecivedEvent: NotificationReceivedEvent) => {
+      console.log(notificationRecivedEvent);
+    })
+
+    return () => unsubscribe;
+
+  },[])
 
   return (
     <NativeBaseProvider theme={THEME}>
