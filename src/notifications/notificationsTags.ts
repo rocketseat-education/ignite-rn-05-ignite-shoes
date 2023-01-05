f97c145382d5dc0633f569a5b614359f566e3e8c
RennanD <rplaykl@gmail.com>
2023-01-05 09:50:42 -0300

    Push Notifications - Removendo Tag

--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -1,5 +1,5 @@
 import OneSignal from 'react-native-onesignal';
 
 export function tagUserEmailCreate(email: string) {
-  OneSignal.sendTag('user_email', email);
+  OneSignal.deleteTag('user_email');
 }
