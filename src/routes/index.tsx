3dceacfd8752754208df3830ca5ddff2eb15d4ff
RennanD <rplaykl@gmail.com>
2023-01-09 11:23:57 -0300

    Deep Linking - Configurando navegação do Deep Linking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,20 @@ import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './app.routes';
 import { Notification } from '../components/Notification';
 
+const linking = {
+  prefixes: ['igniteshoesapp://','com.rocketseat.igniteshoes://'],
+  config: {
+    screens: {
+      details: {
+        path: 'details/:productId',
+        parse: {
+          productId: (productId: string) => productId
+        }
+      }
+    }
+  }
+}
+
 export function Routes() {
   const [notification, setNotification] =  useState<OSNotification>();
 
@@ -27,7 +41,7 @@ export function Routes() {
   },[])
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
       {
         notification?.title &&
