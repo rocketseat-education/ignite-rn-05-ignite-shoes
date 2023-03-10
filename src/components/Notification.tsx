7ca9ed79b82d56a32bda8bfc34a797ce81870142
RennanD <rplaykl@gmail.com>
2023-03-10 10:30:53 -0300

    Deep Linking - Tratando Deep Linking com App em primeiro plano

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,30 +1,20 @@
 import { HStack, Text, IconButton, CloseIcon, Icon, Pressable } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
 import { OSNotification } from 'react-native-onesignal';
+import * as Linking from 'expo-linking';
 
 type Props = {
   data: OSNotification;
   onClose: () => void;
 }
 
-type AditionalDataProps = {
-  route?: 'details';
-  product_id?: string;
-}
-
 export function Notification({ data, onClose }: Props) {
-
-  const { navigate } = useNavigation();
 
   function handleOnPress() {
 
-    const { route, product_id } = data.additionalData as AditionalDataProps
-
-    if(route === 'details' && product_id) {
-      navigate('details', { productId: product_id });
+    if(data.launchURL) {
+      Linking.openURL(data.launchURL);
       onClose();
-
     }
 
   }
